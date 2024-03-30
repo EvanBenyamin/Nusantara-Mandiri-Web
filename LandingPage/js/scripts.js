899f6dc6c8cc4dbe0757c97e77ff7a82ea4467a3
BENJA(-) <101829863+EvanBenyamin@users.noreply.github.com>
2024-03-31 03:00:50 +0700

    branch-start: Jumbotron = carousel
    Start new branch: change jumbotron to carousel

--- LandingPage/js/scripts.js
+++ LandingPage/js/scripts.js
@@ -1,10 +1,23 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+// import Hammer from 'hammerjs';
 
 
 document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new bootstrap.Popover(popover)
   })
+
+//   // initialize Hammer.js on the carousel element
+// var carouselElement = document.getElementById('carouselAutoplaying');
+// var hammer = new Hammer(carouselElement);
+
+//   //add swipe event handlers 
+//   hammer.on('swipeleft', function(){
+//     $('#carouselAutoplaying').carousel('next')
+//   });
+//   hammer.on('swiperight', function(){
+//     $('#carouselAutoplaying').carousel('prev')
+//   });
 
 
 
