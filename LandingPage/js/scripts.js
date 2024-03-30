44d78b1a2b1ca4bf3e269b8e43d8041546366d5f
BENJA(-) <101829863+EvanBenyamin@users.noreply.github.com>
2024-03-30 10:51:27 +0700

    solved directory and dependencies error
    also changed landing page jumbotron bg

--- LandingPage/js/scripts.js
+++ LandingPage/js/scripts.js
@@ -1,12 +1,10 @@
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-document.onload({
-    
-});
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 
 
 document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new bootstrap.Popover(popover)
   })
+
+
 
